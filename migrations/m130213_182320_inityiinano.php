3d8a17ee62efda7da103692e9b93a5e448da5765
slavko <voronenko@gmail.com>
2013-02-13 18:14:31 +0200

    Correct migration for auth module.

--- migrations/m130213_182320_inityiinano.php
+++ migrations/m130213_182320_inityiinano.php
@@ -3,39 +3,54 @@ class m130213_182320_inityiinano extends EDbMigration
 { 
     public function up() 
     {
-        /** @var $configmodule YIIConfigModule */
-        $configmodule = Yii::app()->getModule('config');
-        if ($configmodule == null) {
+        /** @var $configModule YIIConfigModule */
+        $configModule = Yii::app()->getModule('config');
+        if ($configModule == null) {
             throw new exception ("This migration requires YIIConfig module from https://github.com/Voronenko/yii-config");
         }
 
         echo 'congifuring yii-auth';
         //
-        $yiiauthconfig = array(
-            'aliases' => array(
-                'auth' => 'vendor.crisu83.yii-auth'
-             ),
-            'modules' => array(
-                'auth',
+        $yiiauthconfig = array (
+            'aliases' =>
+            array (
+                'auth' => 'vendor.crisu83.yii-auth',
             ),
-            'components' => array(
-                'authManager' => array(
-                    'behaviors' => array(
-                        'auth' => array(
+            'modules' =>
+            array (
+                'auth' => array(
+                    'class'=>'auth.AuthModule'
+                ),
+            ),
+            'components' =>
+            array (
+                'authManager' =>
+                array (
+                    'behaviors' =>
+                    array (
+                        'auth' =>
+                        array (
                             'class' => 'auth.components.AuthBehavior',
-                            'admins' => array('admin', 'foo', 'bar'), // users with full access
+                            'admins' =>
+                            array (
+                                'admin',
+                                'foo',
+                                'bar',
+                            ),
                         ),
                     ),
                 ),
-                'user' => array(
+                'user' =>
+                array (
                     'class' => 'auth.components.AuthWebUser',
                 ),
             ),
         );
 
-        $configmodule->registerConfigUpdate($yiiauthconfig,'congifuring yii-auth');
 
-        $configmodule->writeConfig();
+        $configModule->registerConfigUpdate($yiiauthconfig,'congifuring yii-auth');
+
+        $configModule->writeConfig();
         echo 'done';
 
 
